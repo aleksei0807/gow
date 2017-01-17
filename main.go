4ac2e3cef1cb42848495cb0bcc5aeb03830fc457
Алексей <a@guava.by>
2017-01-18 01:03:29 +0300

    get file paths

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+)
+
+var (
+	path = flag.String("path", "", "path to watch")
+	r    = flag.Bool("r", false, "recursive")
+	ext  = flag.String("ext", "go", `watching file extensions. default: "go"`)
+)
+
+func main() {
+	flag.Parse()
+	if len(*path) > 0 {
+		extArr := strings.Split(*ext, ",")
+		var extMap map[string]bool
+		for _, p := range extArr {
+			if extMap == nil {
+				extMap = make(map[string]bool)
+			}
+			if len(p) > 0 {
+				extMap[strings.TrimSpace(p)] = true
+			}
+		}
+
+		log.Printf("extensions: %v", extMap)
+		watchPath(*path, *r, true, extMap)
+	}
+}
---
+++ watchPath.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	gPath "path"
+	"strings"
+)
+
+func isTrueExt(name string, ext map[string]bool) bool {
+	nameArr := strings.Split(name, ".")
+	myExt := nameArr[len(nameArr)-1]
+	if ext[myExt] {
+		return true
+	}
+	return false
+}
+
+func watchPath(path string, r bool, first bool, ext map[string]bool) {
+	var fullpath string
+	if first {
+		log.Printf("path: %s", path)
+		cmd := exec.Command("sh", "-c", "ls -d -1 $PWD")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fullpath = gPath.Clean(strings.Split(string(out), "\n")[0] + "/" + path)
+		log.Printf("fullpath: %s", fullpath)
+	} else {
+		fullpath = gPath.Clean(path)
+	}
+
+	if first || r {
+		cmd1 := exec.Command("sh", "-c", "ls -1 "+path)
+		out1, err := cmd1.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+		paths := strings.Split(string(out1), "\n")
+		if len(path) > 0 {
+			for _, p := range paths {
+				if len(p) > 0 {
+					myPath := gPath.Clean(fullpath + "/" + p)
+					file, err := os.Open(myPath)
+
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer file.Close()
+
+					fi, err := file.Stat()
+					if err != nil {
+						log.Fatal(err)
+					}
+					if fi.IsDir() {
+						watchPath(myPath, r, false, ext)
+					} else {
+						if len(ext) < 1 || isTrueExt(p, ext) {
+							log.Printf("file: %s", myPath)
+						}
+					}
+				}
+			}
+		}
+	} else {
+		file, err := os.Open(fullpath)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		fi, err := file.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if fi.IsDir() {
+			log.Printf("directory: %s", fullpath)
+		} else {
+			if len(ext) < 1 || isTrueExt(path, ext) {
+				log.Printf("file: %s", fullpath)
+			}
+		}
+	}
+}
